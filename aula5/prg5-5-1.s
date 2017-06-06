ff9ef3219bb6a2342b5d2ae4d26375863e0d2f6b
Leonardo Iacovini <leoiacovini@gmail.com>
2017-06-06 15:39:09 -0300

    Fix 5-5-1

--- aula5/prg5-5-1.s
+++ aula5/prg5-5-1.s
@@ -1,3 +1,7 @@
+.text
+.global main
+
+main:
 		MOV		r3, #0x100
 		MOV		r1, #0x5000
 		STR		r3, [r1], #4
@@ -16,25 +20,26 @@
 		ADD		r3, r3, #0x1
 		STR		r3, [r1], #4
 		ADD		r3, r3, #0x1
-		
+
 		MOV		r0, #8
-		MOV		r1, #0 ;i
-		
-		
+		MOV		r1, #0 @i
+
+
 		MOV		r5, #0x4000
 
-loop
+loop:
 		CMP		r1, #8
 		BGE		done
-		
-		RSB		r6, r1, #7 ;calculando 7 - i
-		LSL		r6, r6, #2 ;multiplicando 7-i por 4
-		ADD		r6, r6, #0x5000 ;somando a base 5000, agora r6 tem o endereço de memória de b[7-i]
-		
-		LDR		r7, [r6], #4 ;carregando valor de b[7-i] em r7
-		STR		r7, [r5], #4 ;guardando em a[i] o valor de b[7-1]
-		
-		
+
+		RSB		r6, r1, #7 @calculando 7 - i
+		MOV		r6, r6, LSL #2 @multiplicando 7-i por 4
+		ADD		r6, r6, #0x5000 @somando a base 5000, agora r6 tem o endereço de memória de b[7-i]
+
+		LDR		r7, [r6], #4 @carregando valor de b[7-i] em r7
+		STR		r7, [r5], #4 @guardando em a[i] o valor de b[7-1]
+
+
 		ADD		r1, r1, #1
 		BAL		loop
-done
+done:
+		SWI 	0x123456
