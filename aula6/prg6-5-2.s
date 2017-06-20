4ee6fadba1f55d4ab831e7bd4be225e5e787c54d
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-06-20 16:40:06 -0300

    Fix for gnu

--- aula6/prg6-5-2.s
+++ aula6/prg6-5-2.s
@@ -1,3 +1,7 @@
+.text
+.global main
+
+main:
 				MOV		r0, #0x4000
 				MOV		r1, #16
 				STRB		r1, [r0], #1
@@ -31,85 +35,85 @@
 				STRB		r1, [r0], #1
 				MOV		r1, #1
 				STRB		r1, [r0], #1
-				
-				
+
+
 				MOV		r1, #4
 				MOV		r11, #0
 				MOV		r10, #0
 				MOV		r12, #34
 				;		r1 * (r1 * r1 + 1) / 2
-loop ; main loop
+loop: ; main loop
 				;r11		percorre inicio das linhas
 				;r10		percorre inicio das colunas
 				BL		checkrow
 				CMP		r9, #0
 				BEQ		not_magic
-				
+
 				BL		checkcolumn
 				CMP		r9, #0
 				BEQ		not_magic
-				
+
 				CMP		r10, #0
 				BLEQ		checkdiagonal1
 				CMP		r9, #0
 				BEQ		not_magic
-				
+
 				CMP		r10, #3
 				BLEQ		checkdiagonal2
 				CMP		r9, #0
 				BEQ		not_magic
-				
+
 				ADD		r11, r11, #4	; percorre inicio linhas
 				ADD		r10, r10, #1    ; percorre inicio de colunas
 				CMP		r11, #16
 				BLT		loop
 				B		done
-				
-				
-checkrow ; funcao que chega linha
+
+
+checkrow: ; funcao que chega linha
 				MOV		r2, r11
 				MOV		r8, #0x4000
 				ADD		r8, r8, r2
-				
+
 				MOV		r3, #0
 				MOV		r5, #0
-loopcheckrow
+loopcheckrow:
 				LDRB		r4, [r8], #1
 				ADD		r5, r5, r4
 				ADD		r3, r3, #1
 				CMP		r3, r1 ; vai até N
 				BLT		loopcheckrow
-				
+
 				CMP		r5, r12
 				MOVEQ	r9, #1
 				MOVNE	r9, #0
 				MOV		pc, r14
-				
-checkcolumn ;funcao que checa coluna
+
+checkcolumn: ;funcao que checa coluna
 				MOV		r2, r10
 				MOV		r8, #0x4000
 				ADD		r8, r8, r2
 				MOV		r3, #0
 				MOV		r5, #0
-loopcheckcolumn
+loopcheckcolumn:
 				LDRB		r4, [r8], r1
 				ADD		r5, r5, r4
 				ADD		r3, r3, #1
 				CMP		r3, r1 ; vai até N
 				BLT		loopcheckcolumn
-				
+
 				CMP		r5, r12
 				MOVEQ	r9, #1
 				MOVNE	r9, #0
 				MOV		pc, r14
-				
-checkdiagonal1
+
+checkdiagonal1:
 				MOV		r3, #0
 				MOV		r4, #0
 				MOV		r8, #0x4000
 				MOV		r6, #0
 				ADD		r6, r1, #1
-loop2
+loop2:
 				LDRB		r5, [r8], r6
 				ADD		r4, r4, r5
 				ADD		r3, r3, #1
@@ -119,15 +123,15 @@ loop2
 				MOVEQ	r9, #1
 				MOVNE	r9, #0
 				MOV		pc, r14
-				
-checkdiagonal2
+
+checkdiagonal2:
 				MOV		r3, #0
 				MOV		r4, #0
 				MOV		r8, #0x4000
 				MOV		r6, #0
 				SUB		r6, r1, #1
 				ADD		r8, r8, r6
-loop3
+loop3:
 				LDRB		r5, [r8], r6
 				ADD		r4, r4, r5
 				ADD		r3, r3, #1
@@ -137,8 +141,8 @@ loop3
 				MOVEQ	r9, #1
 				MOVNE	r9, #0
 				MOV		pc, r14
-				
-not_magic
-				
-done
-				CMP		r7, #34
+
+not_magic:
+
+done:
+	SWI 0x123456
